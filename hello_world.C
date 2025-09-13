654d723848a795c2e6ed9aaff7aee3e018818bf8
Valentine Sidi <valentinesidi1@gmail.com>
2025-09-13 18:38:21 +0300

    Add files via upload
    Added my first program

---
+++ hello_world.C
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world! My name is Valentine.\n");
+    printf("I have just started my journey to become a great Software Engineer.\n");
+    printf("Watch this space as I grow and build amazing projects!\n");
+    return 0;
+}
